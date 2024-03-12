cc0be76fdee5f0c53cfebd74a8efc0f86736a3de
Satvik <satvik.ahuja13@gmail.com>
2024-03-12 23:57:53 +0530

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -1,35 +1,92 @@
-import os
-import sys
+# import os
+# import sys
+# import numpy as np
+# import pandas as pd
+# import dill
+# from sklearn.metrics import r2_score
+# from sklearn.model_selection import GridSearchCV
+#
+# from src.exception import CustomException
+#
+#
+# def save_object(file_path, obj):  # used in data_transformation.py to convert obj to pk1 file
+#     try:
+#         dir_path = os.path.dirname(file_path)
+#
+#         os.makedirs(dir_path, exist_ok=True)
+#
+#         with open(file_path, "wb") as file_obj:
+#             dill.dump(obj, file_obj)
+#
+#     except Exception as e:
+#         raise CustomException(e, sys)
+#
+#
+# def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+#     try:
+#         report = {}
+#         for i in range(len(models)):
+#             model = list(models.values())[i]
+#             para = param[list(models.keys())[i]]
+#
+#             gs = GridSearchCV(model, para, cv=3)
+#             gs.fit(X_train, y_train)
+#
+#             model.set_params(**gs.best_params_)
+#             model.fit(X_train, y_train)
+#
+#             y_train_pred = model.predict(X_train)
+#             y_test_pred = model.predict(X_test)
+#
+#             train_model_score = r2_score(y_train, y_train_pred)
+#             test_model_score = r2_score(y_test, y_test_pred)
+#
+#             report[list(models.keys())[i]] = test_model_score
+#
+#         return report
+#     except Exception as e:
+#         raise CustomException(e, sys)
+#
+# def load_object(file_path):
+#     try:
+#         with open(file_path,"rb") as file_obj:
+#             return dill.load(file_obj)
+#     except Exception as e:
+#         raise CustomException(e, sys)
+#
+# some general code that we will use across the entire project(like reading the dataset)
+import os, sys
+
+import dill
 import numpy as np
 import pandas as pd
-import dill
+
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
 
-def save_object(file_path, obj):  # used in data_transformation.py to convert obj to pk1 file
+def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
 
         os.makedirs(dir_path, exist_ok=True)
 
-        with open(file_path, "wb") as file_obj:
+        with open(file_path, 'wb') as file_obj:
             dill.dump(obj, file_obj)
-
     except Exception as e:
         raise CustomException(e, sys)
 
 
-def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params, cv=3, n_jobs=3, verbose=1, refit=False):
     try:
         report = {}
         for i in range(len(models)):
             model = list(models.values())[i]
-            para = param[list(models.keys())[i]]
+            para = params[list(models.keys())[i]]
 
-            gs = GridSearchCV(model, para, cv=3)
+            gs = GridSearchCV(model, para, cv=cv, n_jobs=n_jobs, verbose=verbose, refit=refit)
             gs.fit(X_train, y_train)
 
             model.set_params(**gs.best_params_)
@@ -46,3 +103,12 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
         return report
     except Exception as e:
         raise CustomException(e, sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
+
