e7b36d0ab39fe0b110e7272545e79aedcf764752
Satvik <satvik.ahuja13@gmail.com>
2024-03-12 14:06:04 +0530

    Hyperparameter training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -34,19 +34,57 @@ class ModelTrainer:
                             test_array[:,:-1],
                             test_array[:,-1],
                         )
-            models= {
+            models = {
                 "Random Forest": RandomForestRegressor(),
-                "Decisiom Tree": DecisionTreeRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
-                "K-Neighbors Classifier": KNeighborsRegressor(),
-                "XGBClassifier": XGBRegressor(),
-                "CatBoosting Classifier": CatBoostRegressor(verbose=False),
-                "AdaBoost Classifier": AdaBoostRegressor(),
+                "XGBRegressor": XGBRegressor(),
+                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor": AdaBoostRegressor(),
             }
 
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
-                                             models=models)            ## to get best model score form dict
+            params={
+                "Random Forest": {
+                    # criterion:['squared_error','friedman_mse','absolute_error','poisson'],
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse','absolute_error','poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+
+                "Gradient Boosting":{
+                    #'loss':['squared_error','huber','absolute_error','quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    #'criterion':['squared_error','friedman_mse'],
+                    #'max_feature':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+
+
+            }
+
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models, param=params)
+            ## to get best model score form dict
 
             best_model_score = max(sorted(model_report.values()))
 
@@ -55,7 +93,7 @@ class ModelTrainer:
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
             ]
-            best_model=models[best_model_name]
+            best_model = models[best_model_name]
 
             if best_model_score<0.6:
                 raise CustomException("No best model found")
@@ -70,7 +108,7 @@ class ModelTrainer:
             return r2_square, best_model_name
 
         except Exception as e:
-            raise CustomException(sys)
+            raise CustomException(e,sys)
 
 
 
